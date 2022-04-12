b362edc027530cdecd95979c9cebc43afc8b8e1f
Saimon Davi <saimon.dsfg@gmail.com>
2022-04-12 18:00:22 -0300

    Exemplo do método POST

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const res = require('express/lib/response')
 
 const app = express()
 
 app.listen('3000')
 
+// middleware
+app.use(express.json())
 
 app.route('/').get((req, res) => {
     res.send('Hello')
@@ -11,4 +14,9 @@ app.route('/').get((req, res) => {
 
 app.route('/about').get((req, res) => {
     res.send('About')
+})
+
+app.route('/').post((req, res) => {
+    console.log(req.body)
+    res.send(req.body)
 })
