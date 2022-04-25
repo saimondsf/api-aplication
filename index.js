a1c7acf3a328999f919b5d8291c7f39c66beb6a4
Saimon Davi <saimon.dsfg@gmail.com>
2022-04-25 16:19:32 -0300

    Parâmetro do Query da Requisição

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ app.listen('3000')
 // middleware
 app.use(express.json())
 
-app.route('/').get((req, res) => res.send("Oi"))
-
-app.route('/:variable').get((req, res) => {
-    res.send(req.params.variable)
+app.route('/').get((req, res) => {
+    res.send(req.query)
 })
